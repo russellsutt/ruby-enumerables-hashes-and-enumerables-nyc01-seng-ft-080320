7193566f288b708096b19a1ac160fcd690efb8ac
Russell Sutter <russell.sutter@gmail.com>
2020-07-14 20:33:52 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,12 +5,5 @@
 # }
 
 def happy_birthday(birthday_kids)
-  winner = ""
-  passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
-    end
-end
- 
-winner
+
 end
